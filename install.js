04f44a42facb40af658cab62b9be04419a23e066
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-02-27 18:30:30 +0000

    Version 0.0.1

---
+++ install.js
@@ -0,0 +1,21 @@
+(function () {
+    var ext = this;
+    ext._shutdown = function () {};
+    ext._getStatus = function () {
+        return {
+            status: 2,
+            msg: 'Ready'
+        };
+    };
+    ext.check = function () {
+      return true;
+    };
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['b', '@greenFlag ScriptExt installed?', 'check'],
+            ['h', 'when @greenFlag ScriptExt installed', 'check']
+        ],
+    };
+    ScratchExtensions.register('ScriptExt', descriptor, ext);
+})();    
