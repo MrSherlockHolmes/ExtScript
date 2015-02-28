76cb30de5d246d9603b4802a386d94d6fe8b70aa
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-02-28 21:35:33 +0000

    Hola World

--- install.js
+++ install.js
@@ -48,3 +48,39 @@
     };
     ScratchExtensions.register('ScriptExt', descriptor, ext);
 })();    
+(function () {
+    var ext = this;
+    ext._shutdown = function () {};
+    ext._getStatus = function () {
+        return {
+            status: 2,
+            msg: 'Ready'
+        };
+    };
+    ext.tobool = function (str) {
+        if (str == '1') {
+            return true
+        } else if (str == 'true') {
+            return true
+        } else if (str == 't') {
+            return true
+        } else if (str == 'yes') {
+            return true
+        } else if (str == 'y') {
+            return true
+        } else {
+            return false
+        }
+    }
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['b', '%s to boolean', 'tobool', 'yes']
+        ],
+    menus: {
+        stuff: ['//add to stuff']
+    },        
+        url: "https://github.com/MrSherlockHolmes/ExtScript/blob/master/README.md"        
+    };
+    ScratchExtensions.register('ScriptExt: Operators', descriptor, ext);
+})();    
