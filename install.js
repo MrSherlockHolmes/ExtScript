d29d717c90727b5cdc48d0ab7532ff8ec40f64e8
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-02-28 21:54:44 +0000

    done

--- install.js
+++ install.js
@@ -10,24 +10,12 @@
     ext.check = function () {
       return true;
     };
-    //Not working :/
-    ext.install = function () {
-        if (library == 'data') {
-            (function(){$.getScript("https://rawgit.com/MrSherlockHolmes/ExtScript/master/install.js");}());
-        } else {
-            (function(){$.getScript("https://rawgit.com/MrSherlockHolmes/ExtScript/master/experimental.js");}());
-        }
-    }
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['b', '@greenFlag ScriptExt installed?', 'check'],
             ['h', 'when @greenFlag ScriptExt installed', 'check'],
-    //[' ', 'load library %m.library', 'install']
         ],
-    menus: {
-        library: ['data', 'experimental']
-    },        
         url: "https://github.com/MrSherlockHolmes/ExtScript/blob/master/README.md"        
     };
     ScratchExtensions.register('ScriptExt', descriptor, ext);
