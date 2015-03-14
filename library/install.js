6a7ef15e33033ef66513346d489d29e73b8173f0
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-14 11:46:26 +0000

    Hola World

--- library/install.js
+++ library/install.js
@@ -17,15 +17,29 @@
             // use proxy [hosted by scratchext]
             jQuery.getScript('http://scratchextproxy.x10.mx/extscript.php?file=library/' + install + '.js');
     }
+    ext.other = function (other, installer) {
+        if (other == 'Alarm Extension') {
+            installer = 'alarm_extension';          
+        } else if (other == 'Browser Stuff') {
+            installer = 'browser_stuff';
+        } else if (other == 'Cool helpful things extension') {
+            installer = 'cool_helpful';
+        } else {
+            installer = 'debugging';
+        }
+            jQuery.getScript('http://scratchextproxy.x10.mx/extscript.php?file=other/' + installer + '.js');        
+    }
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['b', '@greenFlag ScriptExt installed?', 'check'],
             ['h', 'when @greenFlag ScriptExt installed', 'checkhat'],
-            [' ', 'install library %m.install', 'install', 'operators']
+            [' ', 'install library %m.install', 'install', 'operators'],
+            [' ', 'install other library %m.other', 'other', 'Alarm Extension']
         ],
     menus: {
         install: ['operators', 'api'],
+        other: ['Alarm Extension', 'Browser Stuff', 'Cool helpful things extension', 'Debugging']
     },                
         url: "https://github.com/MrSherlockHolmes/ExtScript/blob/master/README.md"        
     };
