042a0914961647db75fd8f243a84fcdba667479a
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-14 11:10:52 +0000

    Update fun.js

--- library/fun.js
+++ library/fun.js
@@ -7,9 +7,23 @@
             msg: 'Installed'
         };
     };
-    ext.query = function(str, resp) {
+    ext.query = function(str, respe) {
      if (str == 'Hi!') {
-     
+        respe = 'Hello!';
+     } else if (str == 'Hello!') {
+         respe = 'Hi!';
+     } else if (str == 'How are you?') {
+         respe = 'Good thanks!';
+     } else if (str == 'Do you like queries?') {
+         respe = 'Yep! Sort of!';
+     } else if (str == 'Are you good at maths?') {
+         respe = 'Unfortuantely not.';
+     } else {
+         respe = 'Sorry?';
+     }
+    }
+    ext.respond = function(respe) {
+        return respe;
     }
     // Block and block menu descriptions
     var descriptor = {
