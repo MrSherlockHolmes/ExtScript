6b6d69b6470924f1ccaf94ed2b40ba0d11a36160
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-14 16:51:48 +0000

    Update ealgase.js

--- library/ealgase.js
+++ library/ealgase.js
@@ -5,14 +5,25 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
+	
+	
+	
+	
+	
 	    ext.color = function(color2) {return(color2);}
+	    ext.pi = function(){return(Math.PI)}
+	
+	
+	
+	
 	    var descriptor = {
         blocks: [
             ['r', '%c', 'color', 0],
-        ],
+			['r','pi','pi']
+			],
         menus: {
-            
+          
         }
     };
-    ScratchExtensions.register('ealgase', descriptor, ext);
+    ScratchExtensions.register('ealgase-ext', descriptor, ext);
 })({}); 
