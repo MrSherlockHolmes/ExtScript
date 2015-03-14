eb218d250fbd64ded9d12b087e57c8eb296f5633
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-14 11:32:30 +0000

    Create debugging.js

---
+++ other/debugging.js
@@ -0,0 +1,28 @@
+/* savaka */
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			[' ', 'comment: %s', 'comment'],
+			[' ', 'print %s to web console', 'debug']
+		]
+	};
+	
+	ext._shutdown = function() {
+		
+	};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'};
+	};
+	
+	ext.comment = function(s) {
+		
+	};
+	
+	ext.debug = function(s) {
+		console.log(s);
+	};
+	
+	ScratchExtensions.register('Debugging', descriptor, ext);
+})();
