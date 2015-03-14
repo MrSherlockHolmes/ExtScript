2b905b9b6907cc2517d985a55e83e5841ed67bd4
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-14 16:37:45 +0000

    not finished

---
+++ library/string_stuff.js
@@ -0,0 +1,23 @@
+(function () {
+    var ext = this;
+    ext._shutdown = function () {};
+    ext._getStatus = function () {
+        return {
+            status: 2,
+            msg: 'Installed'
+        };
+    };
+    ext.removecaps = function(str) {
+    
+    }
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['r', 'Remove caps from %s', 'removecaps', 'Hello!'],
+        ],
+        url: "https://github.com/MrSherlockHolmes/ExtScript/blob/master/README.md"        
+    };
+    
+    ScratchExtensions.unregister('ExtScript: StringStuff');
+    ScratchExtensions.register('StringStuff', descriptor, ext);
+})();
