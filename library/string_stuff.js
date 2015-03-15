d3590086471a860f831ef2633a1a15d60ae27216
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-15 08:47:32 +0000

    test

--- library/string_stuff.js
+++ library/string_stuff.js
@@ -7,8 +7,13 @@
             msg: 'Installed'
         };
     };
-    ext.removecaps = function(str) {
-    
+    ext.removecaps = function(str, i) {
+    for (i = -1; i = str.length; i++) {
+        if (str.charCodeAt(i) > 64 && str.charCodeAt(i) < 91) {
+            str.replace(str.charAt(i), "");
+        }
+    }
+        return str;
     }
     // Block and block menu descriptions
     var descriptor = {
