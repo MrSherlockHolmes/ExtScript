1b25187836cede6cfc5ac4cb1c32f6b764ee96c1
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-15 17:11:22 +0000

    le fix

--- library/string_stuff.js
+++ library/string_stuff.js
@@ -11,18 +11,18 @@
     // block functions
     ext.removecaps = function(str, callback) {
         // simple regular expression
-        callback(str.replace(/[A-Z]/g, '');
+        callback(str.replace(/[A-Z]/g, ''));
     };
     ext.removelowers = function(str, callback) {
         // simple regular expression
-        callback(str.replace(/[a-z]/g, '');
+        callback(str.replace(/[a-z]/g, ''));
     };
     
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['R', 'Remove uppercase from %s', 'removecaps', 'Hello!'],
-            ['R', 'Remove lowercase from %s', 'removelowers', 'Hello!'],
+            ['R', 'Remove lowercase from %s', 'removelowers', 'Hello!']
         ],
         url: "https://github.com/MrSherlockHolmes/ExtScript/blob/master/README.md"        
     };
