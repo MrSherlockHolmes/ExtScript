9bbc51b7ea6e6fd0c1f4d9ddea7a0b6769117373
MrSherlockHolmes <MrSherlockHolmes123@gmail.com>
2015-03-18 17:21:49 +0000

    le fix

--- other/kvaxtension.js
+++ other/kvaxtension.js
@@ -1,2 +1,2 @@
 //Thanks kvackkvack! :P
-ScratchExtensions.loadExternalJs("https://eddb88dd722854829255ed75db01256d69cfa6bb-www.googledrive.com/host/0B4nqFvjFmccQdHVTeXpOOTdqMHM");
+javascript:(function (){var s = document.createElement('script');s.setAttribute('src', 'https://eddb88dd722854829255ed75db01256d69cfa6bb-www.googledrive.com/host/0B4nqFvjFmccQdHVTeXpOOTdqMHM');document.body.appendChild(s);})();
